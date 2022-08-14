287177ba220faf838c509f9ebe215ba019780642
EdisPL <111054925+EdisPL@users.noreply.github.com>
2022-08-14 23:51:46 +0300

    Update Code update for gist.lua

--- Code update for gist.lua
+++ Code update for gist.lua
@@ -1,7 +1,7 @@
 --Change id to the gist id--
 id = "a503ca910916b0df41b4f913fa200178"
 shell.run("delete", "run")
-shell.run("wget","")
+shell.run("wget",id)
 
 --Install this code on CraftOS-PC to update code--
 -- gist get bbd581a343897964423894103a8c43f6 update --
